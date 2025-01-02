a631187726e2ea52108ae97581adf4c3999d783b
JarbasMartins <jarbasmartins0708@gmail.com>
2025-01-02 15:41:20 -0300

    site lanchonete

---
+++ script.js
@@ -0,0 +1,10 @@
+const box = document.getElementById("cax");
+const box3 = document.getElementById("cax3");
+
+box.addEventListener("click", function () {
+    window.location.href = "./hope.html";
+});
+
+box3.addEventListener("click", function () {
+    window.location.href = "./core.html";
+});
